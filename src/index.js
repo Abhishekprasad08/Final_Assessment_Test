d8fa84643ac4785f69a99a5debd5d8e1b58f46b0
Abhishek Prasad <92271780+Abhishekprasad08@users.noreply.github.com>
2023-07-18 11:16:31 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import {ProductProvider} from './context';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <ProductProvider>
+        <Router>
+            <App />
+        </Router>
+    </ProductProvider>,
+     document.getElementById('root'));
+
+serviceWorker.unregister();
